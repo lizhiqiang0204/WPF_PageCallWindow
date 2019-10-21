dcba51a1f0ea0c1cb3b7d1e06db54665c17b3c79
unknown <18518092079@163.com>
2019-10-21 14:35:15 +0800

    first commit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Pages;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void CallFromChild(string name)
+        {
+            MessageBox.Show("Hello," + name + "!");
+        }
+
+        private void InitTab1(object sender, EventArgs e)
+        {
+            Page1 a = new Page1();
+            this.frmPage1.Content = a;
+            a.ParentWindow = this;
+        }
+
+        private void InitTab2(object sender, EventArgs e)
+        {
+            Page2 a = new Page2();
+            this.frmPage2.Content = a;
+            a.ParentWindow = this;
+        }
+
+        private void InitTab3(object sender, EventArgs e)
+        {
+            Page3 a = new Page3();
+            this.frmPage3.Content = a;
+            a.ParentWindow = this;
+        }
+    }
+}
---
+++ WpfApp1/Pages/Page1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Page1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        private MainWindow _parentWin;
+        public MainWindow ParentWindow
+        {
+            get { return _parentWin; }
+            set { _parentWin = value; }
+        }
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ParentWindow.Title = "标题1";
+            ParentWindow.CallFromChild("Page1");
+        }
+    }
+}
